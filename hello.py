19a19f0fa64188ae94ae623b927de2580c6505c7
r-kishore-kumar <kishorekumarace@gmail.com>
2023-08-21 21:15:17 +0530

    initial

---
+++ hello.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template
+
+#create an instance
+app=Flask(__name__)
+#create an route
+@app.route('/home/<name>')
+
+def index(name):
+	return '<h1>Hello {}!</h1>'.format(name)
+
+@app.route('/index')
+def homepage():
+	name='Kishore Kumar'
+	Languages=['HTML','CSS','Python','C Programming','C++','MySQL','Flask']
+	return render_template('index.html',name=name,Languages=Languages)
+
+#Create Custom Error pages
+@app.errorhandler(404)
+
+def page_not_found(e):
+	return render_template('404.html'),404
+
+@app.errorhandler(500)
+
+def page_not_found(e):
+	return render_template('500.html'),500
+
+
+if __name__=="__main__":
+	app.run(debug=True)
