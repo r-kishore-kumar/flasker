7de94e1d6a3dbfdd69f7b0c11dc11d276d882a81
r-kishore-kumar <kishorekumarace@gmail.com>
2023-08-23 22:44:30 +0530

    Wtf Forms Started

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
 from flask import Flask,render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
 
 #create an instance
 app=Flask(__name__)
+app.config['SECRET_KEY']="This is my secret key that is something"
+
+#Create a Form Class
+class NamerForm(FlaskForm):
+	name=StringField("What's Your Name?",validators=[DataRequired()])
+	submit=SubmitField("Submit")
+
 #create an route
 @app.route('/home/<name>')
 
@@ -25,6 +35,16 @@ def page_not_found(e):
 
 def page_not_found(e):
 	return render_template('404.html'),404
+
+@app.route('/name',methods=['GET','POST'])
+def name():
+	name=None
+	form=NamerForm()
+	#Validate Form
+	if form.validate_on_submit():
+		name=form.name.data
+		form.name.data=''
+	return render_template('name.html',name=name,form=form)
 
 
 
