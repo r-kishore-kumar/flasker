614cd1d5dddb67476dca8fff42a2ef773cd03fdb
r-kishore-kumar <kishorekumarace@gmail.com>
2023-09-08 11:13:25 +0530

    Flask Migration

--- hello.py
+++ hello.py
@@ -2,9 +2,11 @@ from flask import Flask,render_template,flash,request
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired
-from flask_sqlalchemy import SQLAlchemy 
-import mysql.connector
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate
+import mysql.connector
+
 
 
 
@@ -15,6 +17,7 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///users.db'
 app.config['SECRET_KEY']="This is my secret key that is something"
 
 db=SQLAlchemy(app)
+migrate=Migrate(app,db)
 #Update Data
 @app.route('/update/<int:id>',methods=['POST','GET'])
 
@@ -24,6 +27,7 @@ def update(id):
 	if request.method=="POST":
 		name_to_update.name=request.form['name']
 		name_to_update.email=request.form['email']
+		name_to_update.favourite_color=request.form['favourite_color']
 		try:
 			db.session.commit()
 			flash("User Updated Successfully!")
@@ -40,6 +44,7 @@ class Users(db.Model):
 	id=db.Column(db.Integer,primary_key=True)
 	name=db.Column(db.String(200),nullable=False)
 	email=db.Column(db.String(120),nullable=False,unique=True)
+	favourite_color=db.Column(db.String(120))
 	date_added=db.Column(db.DateTime,default=datetime.utcnow)
 
 	def __repr__(self):
@@ -54,6 +59,7 @@ class NamerForm(FlaskForm):
 class UserForm(FlaskForm):
 	name=StringField("Name",validators=[DataRequired()])
 	email=StringField("Email",validators=[DataRequired()])
+	favourite_color=StringField("Favourite Color")
 	submit=SubmitField("Submit")
 
 
@@ -99,12 +105,13 @@ def add_user():
 	if form.validate_on_submit():
 		user=Users.query.filter_by(email=form.email.data).first()
 		if user is None:
-			user=Users(name=form.name.data,email=form.email.data)
+			user=Users(name=form.name.data,email=form.email.data,favourite_color=form.favourite_color.data)
 			db.session.add(user)
 			db.session.commit()
 		name=form.name.data
 		form.name.data=''
 		form.email.data=''
+		form.favourite_color.data=''
 		flash('User Added Successfully!!!')
 	our_users=Users.query.order_by(Users.date_added)
 	return render_template('create_user.html',form=form,name=name,our_users=our_users)
